f7a1136ca4df7b8cebbc6a91eb9adc01f1ea66e3
usuario <usuario@10.22.143.30>
2014-10-14 14:47:45 +0100

    first commit

---
+++ src/Kata3histogram.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+import java.util.HashMap;
+
+public class kata3histogram <Type>{
+    public static void main (StringHistogram<Type> build(Type, Integer){
+        return null;
+    }
+}
+}
---
+++ src/kata3histogram/Kata3histogram.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kata3histogram;
+
+/**
+ *
+ * @author usuario
+ */
+public class Kata3histogram {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
